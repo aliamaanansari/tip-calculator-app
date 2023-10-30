e97d320a01a0a36926ee501f9eb451410a3e15df
Ali Aman <aliamaan071@gmail.com>
2023-10-30 15:55:21 +0530

    completeCalculatebill

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const billInput = document.getElementById("billTotalInput");
 const tipInput = document.getElementById("tipInput");
 const numberOfPeopleDiv = document.getElementById("numberOfPeople");
-const perPersonTotal = document.getElementById("perPersonTotal");
+const perPersonTotalDiv = document.getElementById("perPersonTotal");
 
 let numberOfPeople = Number(numberOfPeopleDiv.innerHTML);
 
@@ -10,7 +10,6 @@ const calculateBill = () => {
   const tipPercentage = Number(tipInput.value) / 100;
   const tipAmount = bill * tipPercentage;
   const total = tipAmount + bill;
-
   const perPersonTotal = total / numberOfPeople;
-  console.log({ perPersonTotal });
+  perPersonTotalDiv.innerHTML = `$${perPersonTotal.toFixed(2)}`;
 };
